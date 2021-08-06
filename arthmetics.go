4349c04b3ab024ec53ebaf25cd8c3d485a954aab
Pankaj <synopsys_pankaj@yahoo.co.in>
2021-08-06 11:56:58 +0530

    Create arthmetics.go

---
+++ arthmetics.go
@@ -0,0 +1,9 @@
+package arthmetics
+
+
+func Divide(a, b int) (float64, error) { 
+    if b == 0 { 
+        return 0, errors.New("You cannot divide by zero") 
+    }  
+    return float64(a) / float64(b), nil 
+} 
